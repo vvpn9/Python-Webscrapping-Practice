491d748e2bf4200a0653732354d1cf8837646a5e
vvpn9 <moonshake@foxmail.com>
2021-03-05 16:43:02 +0800

    wip

--- Practice 2 with Scrapy on Douban/douban/douban/spiders/spiders.py
+++ Practice 2 with Scrapy on Douban/douban/douban/spiders/spiders.py
@@ -21,7 +21,8 @@ class DoubamovieSpider(scrapy.Spider):
         for each in movies:
             item['title'] = each.xpath(".//span[@class='title'][1]/text()").extract()[0]
             item['bd'] = each.xpath(".//div[@class='bd']/p[1]/text()").extract()[0]
-            item['star'] = each.xpath(".//div[@class='star']/span[@class='rating_num']/text()").extract()[0]
+            item['star'] = \
+            each.xpath(".//div[@class='star']/span[@class='rating_num']/text()").extract()[0]
             quote = each.xpath(".//span[@class='inq']/text()").extract()
             if len(quote) != 0:
                 item["quote"] = quote[0]
